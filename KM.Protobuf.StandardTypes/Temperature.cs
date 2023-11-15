2ae87622f3566702a099dff568e75ff45fe2cdeb
Keith Mifsud <keith.mifsud@kingmakers.com>
2023-11-15 14:23:37 +0100

    feat: Removed unused item element

--- KM.Protobuf.StandardTypes/Temperature.cs
+++ KM.Protobuf.StandardTypes/Temperature.cs
@@ -1,33 +1,32 @@
 ﻿using System;
 
-namespace KM.Protobuf.StandardTypes
+namespace KM.Protobuf.StandardTypes;
+
+public sealed partial class Temperature
 {
-    public sealed partial class Temperature
+    public Temperature ConvertTo(UnitOfTemperature unitOfTemperature)
     {
-        public Temperature ConvertTo(UnitOfTemperature unitOfTemperature)
+        return new Temperature
         {
-            return new Temperature
+            Unit = unitOfTemperature,
+            Value = (Unit, unitOfTemperature) switch
             {
-                Unit = unitOfTemperature,
-                Value = (Unit, unitOfTemperature) switch
-                {
-                    (UnitOfTemperature.Unspecified, UnitOfTemperature.Unspecified) => Value,
-                    (UnitOfTemperature.Celsius, UnitOfTemperature.Celsius) => Value,
-                    (UnitOfTemperature.Fahrenheit, UnitOfTemperature.Fahrenheit) => Value,
-                    (UnitOfTemperature.Kelvin, UnitOfTemperature.Kelvin) => Value,
+                (UnitOfTemperature.Unspecified, UnitOfTemperature.Unspecified) => Value,
+                (UnitOfTemperature.Celsius, UnitOfTemperature.Celsius) => Value,
+                (UnitOfTemperature.Fahrenheit, UnitOfTemperature.Fahrenheit) => Value,
+                (UnitOfTemperature.Kelvin, UnitOfTemperature.Kelvin) => Value,
 
-                    (UnitOfTemperature.Celsius, UnitOfTemperature.Fahrenheit) => (Value * 9f) / 5f + 32f,
-                    (UnitOfTemperature.Celsius, UnitOfTemperature.Kelvin) => Value + 273.15f,
+                (UnitOfTemperature.Celsius, UnitOfTemperature.Fahrenheit) => (Value * 9f) / 5f + 32f,
+                (UnitOfTemperature.Celsius, UnitOfTemperature.Kelvin) => Value + 273.15f,
 
-                    (UnitOfTemperature.Fahrenheit, UnitOfTemperature.Celsius) => (Value - 32f) * 5f / 9f,
-                    (UnitOfTemperature.Fahrenheit, UnitOfTemperature.Kelvin) => ((Value - 32f) * 5f / 9f) + 273.15f,
+                (UnitOfTemperature.Fahrenheit, UnitOfTemperature.Celsius) => (Value - 32f) * 5f / 9f,
+                (UnitOfTemperature.Fahrenheit, UnitOfTemperature.Kelvin) => ((Value - 32f) * 5f / 9f) + 273.15f,
 
-                    (UnitOfTemperature.Kelvin, UnitOfTemperature.Celsius) => Value - 273.15f,
-                    (UnitOfTemperature.Kelvin, UnitOfTemperature.Fahrenheit) => ((Value - 273.15f) * 9f) / 5f + 32f,
+                (UnitOfTemperature.Kelvin, UnitOfTemperature.Celsius) => Value - 273.15f,
+                (UnitOfTemperature.Kelvin, UnitOfTemperature.Fahrenheit) => ((Value - 273.15f) * 9f) / 5f + 32f,
 
-                    _ => throw new NotSupportedException(),
-                }
-            };
-        }
+                _ => throw new NotSupportedException(),
+            }
+        };
     }
 }
